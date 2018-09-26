49ac1a58027743a796af4a6360b7c784cb605c50
itstc <itsthomaschu@gmail.com>
2018-09-25 19:42:42 -0600

    added library to handle env variable

--- app/containers/config.js
+++ app/containers/config.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = process.env.REACT_APP_SERVER_URL || '';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
+export const SERVER_URL = runtimeEnv().REACT_APP_SERVER_URL || '';
